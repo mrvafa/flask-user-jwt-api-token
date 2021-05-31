0f5496b80bd299b1f1b88f33b5de2d421d6ba95c
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-05-31 17:27:12 +0430

    Create user model

---
+++ project/User/models.py
@@ -0,0 +1,49 @@
+import binascii
+import os
+from datetime import datetime, timedelta
+
+import jwt
+
+from project import db, bcrypt, app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email, password):
+        self.email = email
+        self.username = username
+        self.password = bcrypt.generate_password_hash(
+            password, app.config.get('BCRYPT_LOG_ROUNDS')
+        ).decode()
+        self.registered_on = datetime.now()
+        self.token = binascii.hexlify(os.urandom(20)).decode()
+
+    def encode_auth_token(self):
+        try:
+            payload = {
+                'exp': datetime.utcnow() + timedelta(days=0, minutes=10),
+                'iat': datetime.utcnow(),
+                'sub': self.id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
