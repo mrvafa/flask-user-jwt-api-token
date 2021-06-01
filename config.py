35a7d0ca2f7972d7f5d0094e32b4f4e5b1ca5e1e
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-01 08:46:56 +0430

    Created config file for flask settings

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+basedir = Path(__file__).resolve()
+sqlite_local_base = 'sqlite:///../'
+database_name = 'db.sqlite3'
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'e9da0ef46c177336178515bb')
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = sqlite_local_base + database_name
+
+
+class TestingConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = sqlite_local_base + database_name + '_test'
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
--- project/__init__.py
+++ project/__init__.py
@@ -4,17 +4,19 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-load_dotenv()
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite3'
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app_settings = os.getenv(
+#     'APP_SETTINGS',
+#     'config.DevelopmentConfig'
+# )
+app.config.from_object('config.DevelopmentConfig')
 CORS(app)
 
 db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
+
 from project.User.api.urls import auth_blueprint
+
 app.register_blueprint(auth_blueprint)
